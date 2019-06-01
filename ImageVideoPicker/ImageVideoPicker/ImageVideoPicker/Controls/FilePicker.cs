1b764daa4b71833ad8ed1c3578e9dd89553fb3cd
kumartc <41017720+kumartc@users.noreply.github.com>
2019-06-01 21:51:06 +0530

    Initial Commit
    Initial Commit

---
+++ ImageVideoPicker/ImageVideoPicker/ImageVideoPicker/Controls/FilePicker.cs
@@ -0,0 +1,45 @@
+﻿using ImageVideoPicker.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ImageVideoPicker.Controls
+{
+    public static class FilePicker
+    {
+        public static string GetUniquePath(MediaFile type, string path, string name)
+        {
+            string ext = Path.GetExtension(name);
+            if (ext == string.Empty)
+                ext = ((type == MediaFile.Image) ? ".jpg" : ".mp4");
+
+            name = Path.GetFileNameWithoutExtension(name);
+
+            string nname = name + ext;
+            int i = 1;
+            while (File.Exists(Path.Combine(path, nname)))
+                nname = name + "_" + (i++) + ext;
+
+            return Path.Combine(path, nname);
+        }
+
+
+        public static string GetOutputPath(MediaFile type, string path, string name)
+        {
+            path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), path);
+            Directory.CreateDirectory(path);
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                string timestamp = DateTime.Now.ToString("yyyMMdd_HHmmss");
+                if (type == MediaFile.Image)
+                    name = "IMG_" + timestamp + ".jpg";
+                else
+                    name = "VID_" + timestamp + ".mp4";
+            }
+
+            return Path.Combine(path, GetUniquePath(type, path, name));
+        }
+    }
+}
---
+++ ImageVideoPicker/ImageVideoPicker/ImageVideoPicker/Model/MediaFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageVideoPicker.Model
+{
+    public class Files
+    {
+        public string PreviewPath { get; set; }
+        public string Path { get; set; }
+        public MediaFile Type { get; set; }
+    }
+
+    public enum MediaFile
+    {
+        Image,
+        Video
+    }
+
+   
+}
